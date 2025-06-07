6b50df9f7df5025bd2c0d4f5fbd28e373eeee334
ChrisMightBe <cjaydunphy@gmail.com>
2025-06-07 15:37:55 +0100

    modified:   ch4.js

--- ch4.js
+++ ch4.js
@@ -43,3 +43,24 @@ function rangeStep(start, end, step){
 
 console.log(rangeStep(1, 10, 2)); // [1, 3, 5, 7, 9]
 
+function reverseArray(arr){
+    let result = [];
+    for(let i = arr.length - 1; i>=0; i--)
+    {
+        result.push(arr[i]);
+    }
+    return result;
+}
+console.log(reverseArray([1, 2, 3])); // [3, 2, 1]
+
+function reverseArrayInPlace(arr){
+    for(let i = 0; i < Math.floor(arr.length / 2); i++)
+    {
+        let temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+console.log(reverseArrayInPlace([1, 2, 3])); // [3, 2, 1]
