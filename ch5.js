6fe3195f4b73439bfc4b86eefee789f398b05580
ChrisMightBe <cjaydunphy@gmail.com>
2025-06-07 18:45:16 +0100

    modified:   ch5.js

--- ch5.js
+++ ch5.js
@@ -28,3 +28,45 @@ function every(arr, test){
 console.log(every([1, 2, 3], n => n < 4)); // true
 
 
+function dominantDirection(text) {
+    const scriptCounts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.direction : "none";
+    });
+
+    let dominant = null;
+    for (let [direction, count] of Object.entries(scriptCounts)) {
+        if (dominant === null || count > scriptCounts[dominant]) {
+            dominant = direction;
+        }
+    }
+    return dominant;
+}
+
+function countBy(items, groupName) {
+    let counts = {};
+    for (let item of items) {
+        let name = groupName(item);
+        if (!counts[name]) {
+            counts[name] = 0;
+        }
+        counts[name]++;
+    }
+    return counts;
+}
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => code >= from && code < to)) {
+            return script;
+        }
+    }
+    return null;
+}
+
+const SCRIPTS = [
+    {name: "Latin", direction: "ltr", ranges: [[65, 91], [97, 123]]},
+    {name: "Arabic", direction: "rtl", ranges: [[1536, 1786]]},
+];
+
+console.log(dominantDirection("Hey! مرحبا")); // "ltr" or "rtl" based on the dominant direction
