03d56aaa9811cc1f0130dcc620220a3ff0573a81
ChrisMightBe <cjaydunphy@gmail.com>
2025-06-07 01:03:40 +0100

    modified:   ch4.js

--- ch4.js
+++ ch4.js
@@ -11,3 +11,25 @@ console.log(min(10, 20)); // 10
 console.log(min(20, 10)); // 10
 
 
+function isEven(n) {
+    if(n<0)
+    {
+        n = -n;
+        return isEven(n)
+    }
+    if(n == 0)
+    {
+        return true;
+    }
+    else if(n == 1)
+    {
+        return false;
+    }
+    else
+    {
+        return isEven(n-2);
+    }
+}
+console.log(isEven(50)); // true
+console.log(isEven(75)); // false
+
