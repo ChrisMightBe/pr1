e4022b92070d8ddead206256d421193e40cf3096
ChrisMightBe <cjaydunphy@gmail.com>
2025-06-07 01:06:03 +0100

    modified:   ch4.js

--- ch4.js
+++ ch4.js
@@ -33,3 +33,18 @@ function isEven(n) {
 console.log(isEven(50)); // true
 console.log(isEven(75)); // false
 
+function countBs(str){
+    let count = 0;
+    for(let i = 0; i<str.length; i++)
+    {
+        if(str[i] == "B")
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countBs("BBC")); // 2
+console.log(countBs("kBckB")); // 2
+
