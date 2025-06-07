dc5d4c62f739d1eea30487e26c84e1bb43152320
ChrisMightBe <cjaydunphy@gmail.com>
2025-06-07 22:36:52 +0100

    modified:   learning/ch6.js

--- learning/ch6.js
+++ learning/ch6.js
@@ -21,3 +21,53 @@ console.log(new Vec(1, 2).add(new Vec(2, 3)));
 console.log(new Vec(1, 2).subtract(new Vec(2, 3)));
 console.log(new Vec(3, 4).length);
 
+class Group {
+    constructor() {
+        this.members = [];
+    }
+    add(value) {
+        if (!this.has(value)) {
+            this.members.push(value);
+        }
+    }
+    delete(value) {
+        this.members = this.members.filter(member => member !== value);
+    }
+    static from(iterable) {
+        let group = new Group();
+        for (let value of iterable) {
+            group.add(value);
+        }
+        return group;
+    }
+    has(value) {
+        return this.members.includes(value);
+    }
+    [Symbol.iterator]() {
+        let index = 0;
+        return {
+            next: () => {
+                if (index < this.members.length) {
+                    return { value: this.members[index++], done: false };
+                } else {
+                    return { done: true };
+                }
+            }
+        };
+    }
+
+}
+
+let group = Group.from(["a", "b", "c"]);
+console.log(group.has("a")); // true
+console.log(group.has("d")); // false
+
+group.add("d");
+console.log(group.has("d")); // true
+
+group.delete("a");
+console.log(group.has("a")); // false
+
+for (let value of group) {
+    console.log(value); // "b", "c", "d"
+}
