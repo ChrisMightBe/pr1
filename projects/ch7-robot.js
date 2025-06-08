ce402c2935cc48c39f011f71b7008c65de401654
ChrisMightBe <cjaydunphy@gmail.com>
2025-06-08 13:34:07 +0100

    modified:   projects/ch7-robot.js

--- projects/ch7-robot.js
+++ projects/ch7-robot.js
@@ -46,3 +46,41 @@ class VillageState {
     }
 }
 
+function runRobot(state, robot, memory) { 
+    for(let turn = 0;; turn++){
+        if(state.parcels.length == 0){
+            console.log(`Done in ${turn} turns`);
+            break
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+VillageState.random = function(parcelCount = 5){
+    let parcels = [];
+    for(let i = 0; i < parcelCount; i++){
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do{
+            place = randomPick(Object.keys(roadGraph));
+        }
+        while(place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Post Office", parcels);
+};
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+runRobot(VillageState.random(), randomRobot);
+
